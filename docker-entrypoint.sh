7f6b6dc7946db683aa219d146fc04dc88c8ce421
yarphen <sheremet.mykhaylo@gmail.com>
2018-11-06 13:03:42 +0200

    Add entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo $(node -v)
+# needs defined DB_HOST, ELASTICSEARCH_HOST and REDIS_HOST in environment
+# and wait-port npm package installed
+wait-port "$DB_HOST:${DB_PORT:-5432}" && \
+wait-port "$ELASTICSEARCH_HOST" && \
+wait-port "$REDIS_HOST:${REDIS_PORT:-6379}" && \
+if [[ -v DEBUG ]]; then
+  npm run debug
+else
+  npm start
+fi
